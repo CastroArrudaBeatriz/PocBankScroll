7428714138deb4604982b93b7e1bdb8c1c0c8cab
Beatriz Castro <beatriz_arruda_castro@hotmail.com>
2021-04-22 21:55:22 -0300

    funcionalidade de esconder/exibir

--- PocBankScroll/ViewController.swift
+++ PocBankScroll/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var hiddenCard: UIView!
+    @IBOutlet weak var hiddenCount: UIView!
+    @IBOutlet weak var hiddenLoan: UIView!
+    @IBOutlet weak var hiddenRewards: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func changeVisibility(_ sender: Any) {
+        hiddenCard.isHidden = !hiddenCard.isHidden
+        hiddenCount.isHidden = !hiddenCount.isHidden
+        hiddenLoan.isHidden = !hiddenLoan.isHidden
+        hiddenRewards.isHidden  = !hiddenRewards.isHidden
+        
+    }
+    
 
 }
 
